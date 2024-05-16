2e0eb6b206d1ad316efb933b7a4728c1c8a3d8f8
Olesya Mileshko <mileshko.lesya@gmail.com>
2024-05-16 20:09:36 +0300

    Goal fix

--- expense-tracker-rest-api/src/goal/dto/update-goal.dto.ts
+++ expense-tracker-rest-api/src/goal/dto/update-goal.dto.ts
@@ -1,5 +1,6 @@
 import { IsEnum, IsNumberString, IsOptional, IsString } from 'class-validator';
 import { CurrencyCode } from '../../currency/enums/currency-code.enum';
+import { Transform } from 'class-transformer';
 
 export class UpdateGoalDto {
   @IsString()
@@ -7,6 +8,7 @@ export class UpdateGoalDto {
   name: string;
 
   @IsNumberString()
+  @Transform(({ value }) => Number(value))
   @IsOptional()
   goalAmount: number;
 
@@ -15,13 +17,19 @@ export class UpdateGoalDto {
   currency: CurrencyCode;
 
   @IsOptional()
+  @Transform(({ value }) => new Date(value))
   targetDate: string;
 
   @IsNumberString()
   @IsOptional()
+  @Transform(({ value }) => Number(value))
   depositedAmount: number;
 
   @IsString()
   @IsOptional()
   image: string;
+
+  @IsOptional()
+  @Transform(({ value }) => value === 'true')
+  isCompleted: boolean;
 }
